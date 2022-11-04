164e4d2cafb86ad7568c5c93fb07e1eccb940583
Turan10 <canerturan@outlook.dk>
2022-11-04 19:28:05 +0100

    Tilføjelse d 4-11 til klassediagram

--- SP3 new/src/Account.java
+++ SP3 new/src/Account.java
@@ -3,25 +3,16 @@ import java.util.Scanner;
 
 public class Account extends User{
 
-    private ArrayList<User> users;
+
 
     public Account(int ageOfUser, String userName, String password) {
         super(ageOfUser, userName, password);
     }
 
 
-    public ArrayList<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(ArrayList<User> users) {
-        this.users = users;
-    }
 
     public Scanner getUserInput = new Scanner(System.in);
 
-    public void addUsers(User e){
-        users.add(e);
-    }
+
 
 }
--- SP3 new/src/User.java
+++ SP3 new/src/User.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
+
 public class User extends Main{
 
     int ageOfUser;
     private String userName;
     private String password;
-
+    private ArrayList<User> users;
 
     public User(int ageOfUser, String userName, String password) {
         this.ageOfUser = ageOfUser;
         this.userName = userName;
         this.password = password;
+    }
+
+    public void addUsers(User e){
+        users.add(e);
+    }
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
     }
 
     public String getUserName() {
@@ -31,5 +43,16 @@ public class User extends Main{
 
     public int getAgeOfUser() {
         return ageOfUser;
+    }
+
+
+    public boolean login(String username, String password){
+        for (User u : users){
+            if(u.getUserName() == username && u.getPassword() == password){
+                return true;
+            }
+
+        }
+        return false;
     }
 }
