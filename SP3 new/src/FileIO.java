6243355b96bf69ad015d7ae39e1f516ff4a6d202
Turan10 <canerturan@outlook.dk>
2022-11-05 00:53:13 +0100

    Tilføjelse d 4-11 til klassediagram nat

--- SP3 new/src/FileIO.java
+++ SP3 new/src/FileIO.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class FileIO {
 
 
+
     public ArrayList<String> movieList() {
 
         File movieFile = new File("/Users/turan/Documents/GitHub/SP3/Data/ListOfMovies.txt");
@@ -17,20 +18,20 @@ public class FileIO {
             return getStrings(seriesFile);
         }
 
-    private ArrayList<String> getStrings(File seriesFile) {
-        ArrayList<String> seriesData = new ArrayList<>();
+    private ArrayList<String> getStrings(File mediaFile) {
+        ArrayList<String> mediaData = new ArrayList<>();
 
         try {
-            Scanner input = new Scanner(seriesFile);
+            Scanner input = new Scanner(mediaFile);
             input.nextLine();
 
             while (input.hasNextLine()) {
-                seriesData.add(input.nextLine());
+                mediaData.add(input.nextLine());
             }
         } catch (FileNotFoundException e) {
-            seriesData = null;
+            mediaData = null;
         }
-        return  seriesData;
+        return  mediaData;
     }
 }
 
--- SP3 new/src/MainMenu.java
+++ SP3 new/src/MainMenu.java
@@ -1,8 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainMenu extends Account {
+
+    public ArrayList<String> movieList;
+    public ArrayList<String> seriesList;
 
 
     public MainMenu(int ageOfUser, String userName, String password) {
@@ -17,10 +21,10 @@ public class MainMenu extends Account {
             boolean stopProgram = false;
 
             while (!stopProgram) {
-                System.out.println("Velkommen" + getUserName());
+                System.out.println("Velkommen" + " " + getUserName());
                 System.out.println("Hvad kunne du tænke dig?\n 1. Se en liste over alle film.\n 2. Se en liste over alle serier.\n 3. Se gemte film og serier.\n 4.Søg");
                 Scanner choice = new Scanner(System.in);
-                int newMenuNumberInt;
+
 
                 switch (choice.nextLine()) {
                     case "1":
@@ -31,26 +35,57 @@ public class MainMenu extends Account {
                             FileIO movieList = new FileIO();
 
                             System.out.println(movieList.movieList());
-                        break;}
-
-
-
-                            case "2":
-
-                                while(true){
-                                    FileIO seriesList = new FileIO();
-
-                                    System.out.println(seriesList.seriesList());
-                                    break;}
-
-
-
+                            break;
                         }
+                        break;
+
+
+                    case "2":
+
+                        while (true) {
+                            FileIO seriesList = new FileIO();
+
+                            System.out.println(seriesList.seriesList());
+                            break;
+                        }
+                        break;
+
+                    case "3":
+
+
+                        break;
+
+// IN PROGRESS!
+                    case "4":
+
+                        System.out.println("søger du efter film, eller serie?");
+
+                        switch (choice.nextLine()) {
+                            case "film":
+                                while (true) {
+
+                                    System.out.println("Hvilken film leder du efter?");
+                                    choice.nextLine();
+
+                                    for (String e : movieList) {
+                                        if (e.contains(choice.nextLine())) {
+                                            System.out.println(e);
+                                        }else System.out.println("Den film findes ikke");
+                                    }
+                                }
+                        }
+
                 }
 
             }
         }
     }
+}
+
+
+
+
+
 
 
 
